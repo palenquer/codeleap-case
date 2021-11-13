6c3b6d72394cd4babecd71ff54d927b54c679ac0
Paulo Alenquer <pauloalenquer@gmail.com>
2021-11-13 15:42:13 -0300

    added user types

---
+++ src/types/user.ts
@@ -0,0 +1,11 @@
+export interface IUserState {
+  username: string;
+}
+
+export interface GetState {
+  id: number;
+  username: string;
+  created_datetime: Date;
+  title: string;
+  content: string;
+}
